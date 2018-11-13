8ef7e7bc510f6dd3f50fa3851315a442830a0ba0
guofangchao <guofangchao@daling.com>
2018-11-13 18:39:58 +0800

    添加服务

---
+++ config/default.js
@@ -0,0 +1,15 @@
+/**
+ * 默认配置
+ */
+module.exports = {
+    //项目名称
+    name: "node-config",
+    //中心端口
+    port: 3000,
+    //调试
+    debug: false,
+    //返回对象
+    body: {},
+    //api的前缀
+    prefix: "",
+}
