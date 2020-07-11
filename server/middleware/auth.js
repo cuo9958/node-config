42c4bc9c274a32b77954604388f257a20de4bfcf
guofangchao <guofangchao@daling.com>
2020-07-11 12:19:32 +0800

    鉴权错误

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,14 +2,20 @@
  * 用户鉴权
  */
 const config = require('config');
+const UserService = require('../service/user');
 
-module.exports = async function(ctx, next) {
+module.exports = async function (ctx, next) {
     if (ctx.headers.token === config.get('login.token')) {
         await next();
     } else {
-        ctx.body = {
-            code: 0,
-            msg: '用户未登录'
-        };
+        const res = await UserService.check(ctx.headers.token, ctx.headers.username);
+        if (res) {
+            await next();
+        } else {
+            ctx.body = {
+                code: 0,
+                msg: '用户未登录',
+            };
+        }
     }
 };
