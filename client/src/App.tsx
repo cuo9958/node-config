75434e15a58e2b184a0e8a941ce3395876b5e590
guofangchao <guofangchao@daling.com>
2020-09-09 15:44:05 +0800

    增加新内容

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="contained" color="primary">ceshi</Button>
       </header>
     </div>
   );
