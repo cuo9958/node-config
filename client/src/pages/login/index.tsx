fe635750e29a71e7d57f2586f5a4fa2b43c19e3e
guofangchao <guofangchao@daling.com>
2020-09-11 10:47:49 +0800

    登录界面

--- client/src/pages/login/index.tsx
+++ client/src/pages/login/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import './index.less';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import Checkbox from '@material-ui/core/Checkbox';
+import Button from '@material-ui/core/Button';
+import Link from '@material-ui/core/Link';
+import Footer from '../../plugin/Footer';
 
 export default class extends React.Component {
     render() {
@@ -11,12 +18,62 @@ export default class extends React.Component {
                     <div className="logo">Logo</div>
                     <div className="title">管理系统</div>
                     <div className="item">
-                        <TextField variant="outlined" label="用户名" onChange={this.onInputChange} placeholder="请输入用户名" fullWidth />
+                        <TextField
+                            variant="outlined"
+                            label="用户名"
+                            onChange={this.onInputChange}
+                            placeholder="请输入用户名"
+                            fullWidth
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <AccountCircle></AccountCircle>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
                     </div>
                     <div className="item">
-                        <TextField variant="outlined" label="密码" onChange={this.onInputChange} placeholder="请输入密码" fullWidth />
+                        <TextField
+                            type="password"
+                            variant="outlined"
+                            label="密码"
+                            onChange={this.onInputChange}
+                            placeholder="请输入密码"
+                            fullWidth
+                            InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <VpnKeyIcon></VpnKeyIcon>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </div>
+                    <div className="item-small">
+                        <label>
+                            <Checkbox className="checkbox" color="primary" size="small" />
+                            记住我?
+                        </label>
+                    </div>
+                    <div className="item">
+                        <Button variant="contained" color="primary" fullWidth>
+                            登&nbsp;&nbsp;&nbsp;&nbsp;录
+                        </Button>
+                    </div>
+                    <div className="item flex-right">
+                        <Link
+                            component="button"
+                            variant="body2"
+                            onClick={() => {
+                                console.info("I'm a button.");
+                            }}
+                        >
+                            忘记密码?
+                        </Link>
                     </div>
                 </div>
+                <Footer />
             </Container>
         );
     }
---
+++ client/src/plugin/Footer/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function () {
+    return <footer id="footer">Copyright © Your Website 2020.</footer>;
+}
