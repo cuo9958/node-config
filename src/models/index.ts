8f8dc121bbb2af9b3a1d023718972342f9386f65
guofangchao <guofangchao@daling.com>
2020-09-22 15:12:22 +0800

    删除旧的UI

--- src/models/index.ts
+++
@@ -1,6 +0,0 @@
-import user from "./user";
-
-const stores = {
-    user
-};
-export default stores;
--- src/ts/react.ts
+++
@@ -1,6 +0,0 @@
-export interface iRouter {
-    go(name: string): void;
-    goBack(): void;
-    push(path: string): void;
-    replace(path: string): void;
-}
