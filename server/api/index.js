ae4936e0c4c7a93440ad6f33a8a05086eb45ed0c
guofangchao <guofangchao@daling.com>
2020-09-24 12:12:55 +0800

    增加对外接口

--- server/api/index.js
+++ server/api/index.js
@@ -7,5 +7,6 @@ router.use('/api_config/test', require('./test').routes());
 router.use('/api_config/configs', require('./configs').routes());
 router.use('/api_config/channel', require('./channel').routes());
 router.use('/api_config/user', require('./user').routes());
+router.use('/api_config/resource', require('./resource').routes());
 
 module.exports = router;
